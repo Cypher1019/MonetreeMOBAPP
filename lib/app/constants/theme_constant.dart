279643d95f2c5433707a31d9bfff2f8a2fbbd871
Sauharda Shrestha <111726300+Cypher1019@users.noreply.github.com>
2025-02-02 11:01:39 +0545

    di completed

---
+++ lib/app/constants/theme_constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ThemeConstant {
+  ThemeConstant._();
+  static const Color primaryColor = Colors.blue;
+  static const Color appBarColor = Colors.blue;
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:wise_academy/app/app.dart';
+import 'package:wise_academy/app/di/di.dart';
+import 'package:wise_academy/core/network/hive_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await HiveService().init();
+  await initDependencies();
+  runApp(
+    App(),
+  );
+}
